0dcc4c4605d7abb90dfbe0fdd92105313827d1df
diana m <dianamng@bu.edu>
2025-06-25 22:58:17 -0400

    Ch 6 Functions edit 2

--- ch6/mikeIsANiceGuy.m
+++ ch6/mikeIsANiceGuy.m
@@ -1,15 +1,17 @@
 function stats = mikeIsANiceGuy(data)
-meanval = mean(data);
+meanval = myMean(data);
 medianval = median(data);
 minval = min(data);
 maxval = max(data);
 N = length(data);
 stats = [meanval medianval minval maxval N];
 
-disp(['Mean value is ' mum2str(meanval) '.']);
+disp(['Mean value is ' num2str(meanval) '.']);
 disp(['The median value is ' num2str(medianval) '.']);
 
-function y = myMean(x)
-    y = sum(x)/length(x);
+    function y = myMean(x)
+        y = sum(x)/length(x);
+    end
 end
-end
+
+%%6.10 Ask for help on this..

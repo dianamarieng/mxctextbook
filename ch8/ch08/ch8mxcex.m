96210173cff0e06e9c719c4e2c29bb1abb37db50
diana m <dianamng@bu.edu>
2025-07-04 17:25:25 -0400

    ch8 ex

--- ch8/ch08/ch8mxcex.m
+++ ch8/ch08/ch8mxcex.m
@@ -14,16 +14,27 @@
 % for fi = 1:length(files)
 %     data{fi} = load([filedir files(fi).name]);
 % end
-
-% Importing text files 
-fid = fopen('headache_data.txt','r'); 
-datavar = []; 
-row = 1; 
+ 
+%% 2
+fid = fopen('headache_data.txt','r');
+behavioral_data = zeros(100000,1);
+datarow = 1; 
 while ~feof(fid)
-    aline =fgetl(fid);
-    aline = regexp(aline,'\t','split');
-    datavar(row,:) = cellfun(@str2double,aline);
-    row = row+1; 
+    dataline = fgetl(fid); 
+    dataline = regexp(dataline,'\t','split'); 
+    if ~any(strcmpi('trial',dataline))
+        continue 
+    end 
+    trial_column = find(strcmpi('trial',dataline)); 
+    choice_column = find(strcmpi('choice',dataline)); 
+    rt_column = find(strcmpi('rt',dataline)); 
+    accuracy_column= find(strcmpi('accuracy',dataline)); 
+
+    behavioral_data(datarow,1) = str2double(dataline{trial_column+1});
+    behavioral_data(datarow,2) = str2double(dataline{choice_column+1});
+    behavioral_data(datarow,3) = str2double(dataline{rt_column+1});
+    behavioral_data(datarow,4) = str2double(dataline{accuracy_column+1});
+    datarow = datarow+1; 
 end
 fclose(fid);
-
+behavioral_data = behavioral_data(any(behavioral_data,2),:);

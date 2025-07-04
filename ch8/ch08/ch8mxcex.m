dd91413fc84f5282522d32a91b642f40a5fd45d7
diana m <dianamng@bu.edu>
2025-07-04 16:22:05 -0400

    ch8 ex

--- ch8/ch08/ch8mxcex.m
+++ ch8/ch08/ch8mxcex.m
@@ -1,3 +1,8 @@
 % Chapter 8 Examples & Exercises 
 % Edited: 7/4/2025
 datafile = load('ch08filename.mat');
+data = cell(15,1);
+for fi = 1:15 
+     % only loads data from rat files, so data{1} would show the data for rat 1
+    data{fi} = load(['data_rat' num2str(fi) '.mat']);
+end

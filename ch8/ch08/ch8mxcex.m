2645a68d0f0d05057581c409d88d5ddf2b6e37a8
diana m <dianamng@bu.edu>
2025-07-04 16:32:19 -0400

    ch8 ex

--- ch8/ch08/ch8mxcex.m
+++ ch8/ch08/ch8mxcex.m
@@ -1,8 +1,16 @@
 % Chapter 8 Examples & Exercises 
 % Edited: 7/4/2025
-datafile = load('ch08filename.mat');
-data = cell(15,1);
-for fi = 1:15 
-     % only loads data from rat files, so data{1} would show the data for rat 1
-    data{fi} = load(['data_rat' num2str(fi) '.mat']);
+% datafile = load('ch08filename.mat');
+% data = cell(15,1);
+% for fi = 1:15 
+%      % only loads data from rat files, so data{1} would show the data for rat 1
+%     data{fi} = load(['data_rat' num2str(fi) '.mat']);
+% end
+
+% loads files with rat in current directory
+filedir = 'C:\Users\Diana Gamboa\OneDrive\Documents\MATLAB\Denison Lab\ch8\ch08\';
+files = dir([filedir '*rat*.mat']);
+data = cell(size(files)); 
+for fi = 1:length(files)
+    data{fi} = load([filedir files(fi).name]);
 end

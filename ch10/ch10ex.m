0a69dc6ff785c4839184c74d1450a7616c70666c
diana m <dianamng@bu.edu>
2025-07-05 16:06:08 -0400

    ch10 ex 2

--- ch10/ch10ex.m
+++ ch10/ch10ex.m
@@ -10,3 +10,14 @@
 % h) Invalid
 
 %%2
+a = [1 -3];
+b = [3 1];
+% dot product = (1)(3) + (-3)(1) 
+figure
+plot([0 1], [0 -3]); 
+hold on
+plot([0 3], [0 1]);
+xlim([0 3])
+ylim([-3 3])
+% no, it can only be orthoganol is the dot product is equal to zero,
+% |a||b|cos0 where cosA = 0 and hte angle A is equal to 90

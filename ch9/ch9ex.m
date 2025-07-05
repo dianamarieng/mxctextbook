810feb2af4075388b0fce8000f4e484d5bb0d78f
diana m <dianamng@bu.edu>
2025-07-04 20:46:03 -0400

    CH 9 EX

--- ch9/ch9ex.m
+++ ch9/ch9ex.m
@@ -4,3 +4,23 @@ frate = linspace(10,40,n) + 10*rand(1,n);
 fvar = frate + 5*randn(1,n);
 ndepth = linspace(100,1000,n);
 scatter(frate,fvar,100,ndepth,'filled');
+
+%% 9.5 Histogram 
+r = randn(1000,1);
+ru = rand(1000,1);
+[y_r,x_r] = hist(r,50); % y outputs first
+[y_ru,x_ru] = hist(ru,50);
+plot(x_r,y_r,'k','linew',2) % linew = linewidth
+hold on
+plot(x_ru,y_ru,'r','linew',2)
+legend({ 'randn';'rand' })
+
+%% 9.7 Patch 
+x = [1 2 3 4 3 2 1];
+y = [9 9 7 4 1 3 2];
+[~,idx] = sort(x); %sorted in ascending order
+patch(x(idx),y(idx),'r')
+
+%% 9.8 Images
+im = [1 3 4; 0 4 6];
+imagesc(im)

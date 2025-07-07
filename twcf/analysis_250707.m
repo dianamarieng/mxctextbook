8a77262b05faab3a30a80fe2e0a83912133a43b8
diana m <dianamng@bu.edu>
2025-07-07 01:12:54 -0400

    Added subID for subjects completely finished, edited analysis to reflect subject ID

---
+++ twcf/analysis_250707.m
@@ -0,0 +1,9 @@
+% Started: 7/7/2025
+% Last edited: 7/6/202
+% Description: More analysis on TWCF data 
+% Analysis: 
+% S0148, S0002, S0151, S0097, S0004, S0144, S0018, S0108, S0153
+
+% List of subjects with completed tests 
+subID = readmatrix("subID.txt","Delimiter","\n","OutputType","string"); 
+% example directory: C:\Users\Diana Gamboa\OneDrive\Documents\GitHub\twcf\twcf_expt1_data_BU\twcf_cue_tex_det_fmri\S0144\main_expt

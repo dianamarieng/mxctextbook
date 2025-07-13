20d343cf482bd775d2d3b4bce834085d47ad6de7
diana m <dianamng@bu.edu>
2025-07-12 23:41:46 -0400

    added 11.1 slow fourier transform example

--- ch11/ch11ex.m
+++ ch11/ch11ex.m
@@ -10,3 +10,12 @@ plot(time,sinewave)
 % Complex Sine Wave
 csw = exp(1i*2*pi*f*time); 
 plot3(time,real(csw),imag(csw))
+%11.1 
+% Slow Fourier Transofmr 
+% N = length(signal);
+% fTime = (0:N-1)/N;
+% for fi=1:N
+% fSine = exp(-1i*2*pi*(fi-1).*fTime);
+% signalX(fi) = sum(fSine.*signal);
+% end
+% signalX = signalX / N;

6351b8f637dcedf769636520b9c6a3a47a53af04
diana m <dianamng@bu.edu>
2025-07-25 21:22:17 -0400

    twcf analysis

--- twcf/analysis_250725.m
+++ twcf/analysis_250725.m
@@ -5,31 +5,35 @@
 % Analysis: 
 % S0148, S0002, S0151, S0097, S0004, S0144, S0018, S0108, S0153
 
-% File Directory Format
-% Sample file directory: C:\Users\Diana
-% Gamboa\OneDrive\Documents\GitHub\twcf\twcf_expt1_data_BU\twcf_cue_tex_det_fmri\S0004\main_expt\twcf_cue_tex_det_fmri_BU_S0004_main_expt_20250501_094222.mat
+%% Tasks 
+% 1) Create a structure that has fields with the names of each subject -
+% 2) Initialize an array within each subject that has a 20 rows, and a
+% columns of NaNs (which will be trimmed later) -
+% 3) Loop through each subject, updating the array above with the block
+% data of each subject 
+% 4) After all loops, trim NaNs
+
+
+%% Code
 fileSampleDir = 'C:\Users\Diana Gamboa\OneDrive\Documents\GitHub\twcf\twcf_expt1_data_BU\twcf_cue_tex_det_fmri\S0000\main_expt\twcf_cue_tex_det_fmri_BU_S0000_main_expt_*_*.mat';
-
-% List of subjects with completed tests 
+blockN = 20; 
 subID = readmatrix("subID.txt","Delimiter","\n","OutputType","char"); 
-dataTable = [(1:blockN)',zeros(blockN,1)]; 
-
+subjectDataT = struct();
 
 for fi = 1:length(subID)
     % replaces S0000 in sample file directory string with actual subject ID
     fileDirName = strrep(fileSampleDir,'S0000',subID(fi));
     % gets name of file in subject ID folder
     fileName = dir(string(fileDirName)).name; % janky, how to fix?
-    % load data of subject
-    % fileDirName 1:110 is the name of the directory folder is in
+    % load data of subject, fileDirName 1:110 is the name of the directory folder is in
     load([fileDirName{1}(1:110) fileName],"data");
-    % loop through each block and generates block 
-    dataTable = calculateSumRT(data,dataTable);
-    % dataTable(blockIDX,2) = dataTable(blockIDX,2) + sumRT; % adds sumRT to RT across subjects
+    subjectDataT(fi).name = subID{fi}; % loads string of subject ID
+    % creates fields in subject Data 
+    subjectDataT(fi).blockArray = [(1:blockN)',NaN(blockN,100)];
+    subjectDataT(fi) = updateBlockData(data,subjectDataT(fi));
 end
-dataTable(:,2) = dataTable(:,2)/(80*9);
 
-% Reaction Time Plot
+%Reaction Time Plot
 figure
 x = dataTable(:,1); 
 y = dataTable(:,2);
@@ -39,17 +43,18 @@ ylabel('average RT (ms)')
 disp(dataTable)
 
 
-function dataT = calculateSumRT(data,dataT)
-% Description: calculates the total sum reaction time across all subjects
-% and updates the data table 
-% Inputs: structure data contains the loaded data subject's RT, dataT is a
-% matrix of average RT against block number 
-% Ouput: dataT is updated with new total sum 
+function subjectData = updateBlockData(data,subjectData)
+% Desc: Updates subjectData to reflect block data of each participants 
+% Inputs: structure data contains RT from the raw TWCF data mat file,
+% subjectDataField is a structure that contains fields named with the subject ID
+% name and a NaN block array 
+% Output: Updated subjectDataT with updated RT for each participant
+disp(subjectData)
     for i = 1:max(data.i_block)
         idx = data.i_block==i; % finds indices with the current block number
         val = data.RT(idx); % val is an array with all the RT values in this block 
-        dataT(i,2) = dataT(i,2)+sum(val,"omitnan");
+        subjectData.blockArray(i,2:length(val)+1) = val; % assigns block values to rows 2 and onwards
     end
 end
 
-    
+   

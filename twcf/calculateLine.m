c3fc2a683ffba715a3fec0950cc8f47432bc99ad
diana m <dianamng@bu.edu>
2025-08-05 19:49:10 -0400

    More TWCF analysis & poking
    TODO:
    - Need to replace hardcoding with something more dynamic
    - Need to make a function for adding averages to the end of tables
    - Need to create data tables from matrices
    - Need to edit repetitive lines within the calculate functions

--- twcf/calculateLine.m
+++ twcf/calculateLine.m
@@ -1,9 +1,20 @@
 function subArray = calculateLine(data, subArray, subCol)
-    lineN = size(lineArray,1);
-    for linei = 1:max(lineN)
+    for linei = 1:5 % NOT GOOD TO HARD CODE BUT ILL LET IT PASS
+        % valid 
         idxValid = data.stimID_postcue>0 & data.cueValidity==1 & data.lineLengthID_postcue == linei;
         valid = data.correctDis(idxValid);
         validP = mean(valid); % calculates the probability (how again?)
-        subArray()
+        subArray(linei,subCol+1) = validP; 
+        subArray(linei,1) = linei;
+
+        % invalid
+        idxInvalid = data.stimID_postcue>0 & data.cueValidity==-1 & data.lineLengthID_postcue == linei;
+        invalid = data.correctDis(idxInvalid);
+        invalidP = mean(invalid); % calculates the probability (how again?)
+        subArray(linei+5,subCol+1) = invalidP; 
+        subArray(linei+5,1) = linei;
     end
 end
+
+% Unattended vs attended accuracy by block # 
+% Attention effects on the postcue quadrant
